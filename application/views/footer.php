b51745b215ed84d9c98406cb7f08dc550b31149b
Nguyễn Thuý Linh <64420667+Lyyynt@users.noreply.github.com>
2021-05-18 00:09:21 +0700

    Commit

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,3 @@
-
 <?php if (!defined('IN_SITE')) die ('The request not found'); ?>
 </div>
         </div>
@@ -24,8 +23,11 @@
         </div>
 
         <div class="footer">
-        <h4>Công nghệ Web và dịch vụ trực tuyến</h4>
-
+        <h4 style="color:white">Tổ chức giáo dục quốc tế CLV</h4>
+        <h6 style="color:white">Cam kết sẽ đồng hành cùng bạn</h6> 
+        <a target="_blank" href="https://www.facebook.com/profile.php?id=100007623165499"><img src="https://i.imgur.com/wcWc49I.png" alt="Facebook" width="20"/></a>
+        <a target="_blank" href="https://www.instagram.com/redamancy_3008/?hl=fr"><img src="https://i.imgur.com/Y1YVGqh.png" alt="Instagram" width="22"/></a>
+        <a target="_blank" href="https://www.youtube.com/channel/UCAtTO7pMyQBiZziD_91KNqQ"><img src="https://i.imgur.com/9H9Hfb6.png" alt="Instagram" width="24"/></a>
         </div>
 
     </body>
